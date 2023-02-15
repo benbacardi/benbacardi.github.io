f223ad48158adcebaf0cd2250e7142b0da4c3805
Ben Cardy <ben.cardy@ocado.com>
2023-02-15 15:23:10 +0000

    Revert "Use different action"
    This reverts commit 2e561a1f8bd76c8ffac67078bdb3739f62001726.

--- publishconf.py
+++ publishconf.py
@@ -10,6 +10,9 @@ from pelicanconf import *
 SITEURL = 'https://bencardy.co.uk'
 RELATIVE_URLS = False
 
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
